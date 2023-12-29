3d65a68cacfa75e694d1059e96ea0935bb1515cb
urn216 <willkilty@gmail.com>
2023-12-29 22:47:49 +1300

    Triangle clipping (incomplete) and a flashlight

--- src/code/rendering/renderers/ProjectionRenderer.java
+++ src/code/rendering/renderers/ProjectionRenderer.java
@@ -1,5 +1,6 @@
 package code.rendering.renderers;
 
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import mki.math.MathHelp;
@@ -22,6 +23,7 @@ class ProjectionRenderer extends Renderer {
   private static final double lightDistSquared = new Vector3(4000, 10000, 1000).magsquare();
   private static final Vector3 lightDir = new Vector3(0.4, 1, 0.1).unitize();
   private static final Vector3 lightCol = new Vector3(Integer.MAX_VALUE);
+  private static final Vector3 glowLCol  = new Vector3(25, 25, 50);
 
   private double f;
 
@@ -35,10 +37,6 @@ class ProjectionRenderer extends Renderer {
     clippingPlanes[1] = new Vector3(-cos, 0, sin).unitize();
     clippingPlanes[2] = new Vector3(0,  cos, sin*height/width).unitize();
     clippingPlanes[3] = new Vector3(0, -cos, sin*height/width).unitize();
-
-    for (int i = 0; i < clippingPlanes.length; i++) {
-      System.out.println(clippingPlanes[i]);
-    }
   }
 
   @Override
@@ -47,42 +45,80 @@ class ProjectionRenderer extends Renderer {
 
     Quaternion worldRotation = cameraRotation.reverse();
 
+    Vector3 lightDir = worldRotation.rotate(ProjectionRenderer.lightDir);
+
+    //drawing objects
     Stream.of(bodies).parallel().forEach((b) -> {
       Vector3 offset = b.getPosition().subtract(cameraPosition);
-      Vector3 rotoff = worldRotation.rotate(offset);
-      if (
-        rotoff.z - nearClippingPlane  > b.getModel().getRadius() &&
-        rotoff.dot(clippingPlanes[0]) > b.getModel().getRadius() &&
-        rotoff.dot(clippingPlanes[1]) > b.getModel().getRadius() &&
-        rotoff.dot(clippingPlanes[2]) > b.getModel().getRadius() &&
-        rotoff.dot(clippingPlanes[3]) > b.getModel().getRadius()
-      )
-        Stream.of(b.getModel().getFaces()).parallel().forEach((tri) -> renderTri(d, tri, offset, worldRotation, b.getModel().getMat()));
+      Vector3 offrot = worldRotation.rotate(offset);
+
+      //clipping object
+      double rad  = b.getModel().getRadius();
+      boolean partial = false;
+      
+      double clip = offrot.z - nearClippingPlane;
+      if (clip < -rad) return;
+      if (clip <= rad) partial = true;
+
+      for (int i = 0; i < 4; i++) {
+        clip = offrot.dot(clippingPlanes[i]);
+        if (clip < -rad) return;
+        if (clip <= rad) partial = true;
+      }
+      
+      //drawing tris
+      Stream<Tri3D> s = Stream.of(b.getModel().getFaces()).parallel()
+      .filter((tri) -> tri.getVerts()[0].add(offset).dot(tri.getNormal()) < -0.01)
+      .map((tri) -> new Tri3D(new Vector3[]{
+          worldRotation.rotate(tri.getVerts()[0].add(offset)),
+          worldRotation.rotate(tri.getVerts()[1].add(offset)),
+          worldRotation.rotate(tri.getVerts()[2].add(offset))
+        }, tri.getVertUVs(), tri.getVertexIndeces(), tri.getVertexTextureIndeces()));
+
+      if (partial) s = s.<Tri3D>mapMulti(this::clipTri);
+      
+      s.forEach((tri) -> renderTri(d, tri, lightDir, b.getModel().getMat()));
     });
   }
 
-  private void renderTri(Drawing d, Tri3D tri, Vector3 offset, Quaternion worldRotation, Material mat) {
-    Vector3 toTri = tri.getVerts()[0].add(offset);
+  private void clipTri(Tri3D tri, Consumer<Tri3D> c) {
+    for (int i = 0; i < 3; i++) {
+      Vector3 vertex = tri.getVerts()[i];
 
-    if (toTri.dot(tri.getNormal()) >= -0.01) return;
+      double clip = vertex.z - nearClippingPlane;
+      if (clip < 0) return;
 
-    Tri3D projectedTri = projectTri(tri, offset, worldRotation, d.getWidth(), d.getHeight(), d.getAspectRatio());
-    int colour = mat.getIntenseColour(lightCol.scale(MathHelp.intensity(Math.max(tri.getNormal().dot(lightDir), 0), lightDistSquared)));
-    d.fillTri(projectedTri, colour);
-    // d.drawTri(projectedTri, -16777216|~colour);
+      for (int j = 0; j < 4; j++) {
+        clip = vertex.dot(clippingPlanes[j]);
+        if (clip < 0) return;
+      }
+    }
+    c.accept(tri); //AAAAAAHHHH
   }
 
-  private Tri3D projectTri(Tri3D triWorld, Vector3 offset, Quaternion worldRotation, int width, int height, double aspRat) {
-    return new Tri3D(
-      new Vector3[] {
-        projectVector3(worldRotation.rotate(triWorld.getVerts()[0].add(offset)), aspRat).add(1, 1, 0).scale(0.5*width-1, 0.5*height-1, 1),
-        projectVector3(worldRotation.rotate(triWorld.getVerts()[1].add(offset)), aspRat).add(1, 1, 0).scale(0.5*width-1, 0.5*height-1, 1),
-        projectVector3(worldRotation.rotate(triWorld.getVerts()[2].add(offset)), aspRat).add(1, 1, 0).scale(0.5*width-1, 0.5*height-1, 1)
-      }, 
-      triWorld.getVertUVs(), 
-      new int[3],
-      new int[3]
+  private void renderTri(Drawing d, Tri3D tri, Vector3 lightDir, Material mat) {
+    int width  = d.getWidth ();
+    int height = d.getHeight();
+
+    double aspRat = d.getAspectRatio();
+
+    Vector3[] verts = tri.getVerts();
+    Vector3 normal = tri.getNormal();
+    Vector3 vert0  = verts[0].scale(-1).unitize();
+    double distSquared = verts[0].magsquare();
+
+    tri.setVerts(
+      projectVector3(verts[0], aspRat).add(1, 1, 0).scale(0.5*width-1, 0.5*height-1, 1),
+      projectVector3(verts[1], aspRat).add(1, 1, 0).scale(0.5*width-1, 0.5*height-1, 1),
+      projectVector3(verts[2], aspRat).add(1, 1, 0).scale(0.5*width-1, 0.5*height-1, 1)
     );
+
+    int colour = mat.getIntenseColour(
+      lightCol.scale(MathHelp.intensity(Math.max(normal.dot(lightDir), 0), lightDistSquared)).add(
+      glowLCol.scale(MathHelp.intensity(Math.max(normal.dot(vert0   ), 0),      distSquared))
+    ));
+    d.fillTri(tri, colour);
+    // d.drawTri(tri, -16777216|~colour);
   }
 
   private Vector3 projectVector3(Vector3 vecWorld, double aspRat) {
